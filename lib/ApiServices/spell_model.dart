21fc459635e46d84a8c75d122d80fd8edb8d7a80
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-17 18:11:34 +0200

    updated spell model

--- lib/ApiServices/spell_model.dart
+++ lib/ApiServices/spell_model.dart
@@ -1,21 +1,49 @@
 
 
-class SpellModel {
-  //String? id;
-  String name;
-  String description;
+// class SpellModel {
 
-  SpellModel({required this.name,required this.description});
+//   //String? id;
+//   String name;
+//   String description;
 
-  factory SpellModel.fromJson(json) {
-   return SpellModel(
-    name : json[21]['name'],
-    description : json[21]['description']
-    );
+//   SpellModel({required this.name,required this.description});
+
+//   factory SpellModel.fromJson(json) {
+
+// //     List<SpellModel> models =[];
+// // for(int i=0;i<25;i++){
+// //     models.add (SpellModel(
+// //      name : json[i]['name'],
+// //     description : json[i]['description']
+// //     )) ;
+      
+// //     }
+
+
+
+
+//     return  SpellModel(name: json[10]['name'], description: json[10]['description']);
+
+
     
-  }
+    
+//   }
 
   
 
 
+// }
+
+class SpellModel {
+  String name;
+  String description;
+
+  SpellModel({required this.name, required this.description});
+
+  factory SpellModel.fromJson(Map<String, dynamic> json) {
+    return SpellModel(
+      name: json['name'],
+      description: json['description'],
+    );
+  }
 }
