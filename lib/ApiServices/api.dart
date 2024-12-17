b77648a20cf0228f7cb4e781876b278678109b72
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-17 18:12:17 +0200

    updated apiservices

--- lib/ApiServices/api.dart
+++ lib/ApiServices/api.dart
@@ -1,31 +1,57 @@
+// import 'package:dio/dio.dart';
+// import 'package:harry_potter_spells/ApiServices/spell_model.dart';
+
+
+// class ApiProvider{
+
+
+
+//   Future<SpellModel> getSpell() async{
+
+//     try{
+// Response response= await Dio().get("https://hp-api.onrender.com/api/spells");
+
+// print(response.statusCode);
+// print(response.data);
+
+//     SpellModel spellModel = SpellModel.fromJson(response.data);
+//     print(spellModel);
+//   return spellModel;
+//     } on DioException catch(e){
+//    final String errorMessage = e.response?.data ?? " ps there an error , try later";
+//    throw Exception(errorMessage);
+// }
+//  catch(e){
+//   throw Exception(" try later");
+//  }
+
+    
+//   }
+  
+// }
+
 import 'package:dio/dio.dart';
 import 'package:harry_potter_spells/ApiServices/spell_model.dart';
 
+class ApiProvider {
+  Future<List<SpellModel>> getSpells() async {
+    try {
+      Response response = await Dio().get("https://hp-api.onrender.com/api/spells");
 
-class ApiProvider{
+      print(response.statusCode);
+      print(response.data);
 
+      // Map the response data to a list of SpellModel
+      List<SpellModel> spells = (response.data as List)
+          .map((spellJson) => SpellModel.fromJson(spellJson))
+          .toList();
 
-
-  Future<SpellModel> getSpell() async{
-
-    try{
-Response response= await Dio().get("https://hp-api.onrender.com/api/spells");
-
-print(response.statusCode);
-print(response.data);
-
-    SpellModel spellModel = SpellModel.fromJson(response.data);
-    print(spellModel);
-  return spellModel;
-    } on DioException catch(e){
-   final String errorMessage = e.response?.data ?? " ps there an error , try later";
-   throw Exception(errorMessage);
-}
- catch(e){
-  throw Exception(" try later");
- }
-
-    
+      return spells;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data ?? "There was an error, try later";
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception("Try later");
+    }
   }
-  
 }
