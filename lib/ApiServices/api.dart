c77f0275bbb357500a2df7394b706fc641ea259f
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-23 23:34:33 +0200

    use final string api_url

--- lib/ApiServices/api.dart
+++ lib/ApiServices/api.dart
@@ -1,10 +1,7 @@
 // import 'package:dio/dio.dart';
 // import 'package:harry_potter_spells/ApiServices/spell_model.dart';
 
-
 // class ApiProvider{
-
-
 
 //   Future<SpellModel> getSpell() async{
 
@@ -25,9 +22,8 @@
 //   throw Exception(" try later");
 //  }
 
-    
 //   }
-  
+
 // }
 
 import 'package:dio/dio.dart';
@@ -35,9 +31,10 @@ import 'package:flutter/foundation.dart';
 import 'package:harry_potter_spells/ApiServices/spell_model.dart';
 
 class ApiProvider {
+  final String apiUrl = "https://hp-api.onrender.com/api";
   Future<List<SpellModel>> getSpells() async {
     try {
-      Response response = await Dio().get("https://hp-api.onrender.com/api/spells");
+      Response response = await Dio().get("$apiUrl/spells");
 
       if (kDebugMode) {
         print(response.statusCode);
@@ -47,18 +44,19 @@ class ApiProvider {
       }
 
       // Map the response data to a list of SpellModel
-      List<SpellModel> spells =[];
-      for(var spellJson in response.data ){
+      List<SpellModel> spells = [];
+      for (var spellJson in response.data) {
         spells.add(SpellModel.fromJson(spellJson));
       }
-      
+
       // List<SpellModel> spells = (response.data as List)
       //     .map((spellJson) => SpellModel.fromJson(spellJson))
       //     .toList();
 
       return spells;
     } on DioException catch (e) {
-      final String errorMessage = e.response?.data ?? "There was an error, try later";
+      final String errorMessage =
+          e.response?.data ?? "There was an error, try later";
       throw Exception(errorMessage);
     } catch (e) {
       throw Exception("Try later");
