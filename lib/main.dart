47ea0bc012eea1f71991ea28c0d10e9e1a97b865
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-23 23:19:09 +0200

    add blocprovide to materialapp widget not rinapp method

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:harry_potter_spells/Home.dart';
-
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:harry_potter_spells/ApiServices/api.dart';
+//import 'package:harry_potter_spells/Home.dart';
+import 'package:harry_potter_spells/bloc/spell_bloc.dart';
+import 'package:harry_potter_spells/new_home_bloc.dart';
 
 void main() async {
- // print("before");
- //await ApiProvider().getSpell();
+  // print("before");
+  //await ApiProvider().getSpell();
 // print("after");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter App!!',
-      
-      
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
+    return BlocProvider(
+      create: (context) => SpellCubit(ApiProvider()),
+      child: MaterialApp(
+        title: 'Flutter App!!',
+        home: NewHomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
